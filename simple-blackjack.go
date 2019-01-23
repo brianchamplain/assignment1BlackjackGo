9543638231d78704eaecbbdffbcdb3a928cfaa95
Jake R <25373505+Jacob-Rose@users.noreply.github.com>
2019-01-22 22:23:50 -0500

    Finished most definitions except main
    I felt the workload was unbalanced so I did a bit more than asked, not tested code so needs a run through and possible cleanup

--- simple-blackjack.go
+++ simple-blackjack.go
@@ -13,21 +13,57 @@ import (
 	"time"
 )
 
+//used in calculate
+const valueMap map[string]int = {
+	"2" : 2,
+	"3" : 3,
+	"4" : 4,
+	"5" : 5,
+	"6" : 6,
+	"7" : 7,
+	"8" : 8,
+	"9" : 9,
+	"10":10,
+	"J" :10,
+	"Q" :10,
+	"K" :10,
+	"A" :11 //is checked in calculate score directly
+}
+
 // Move a card from deck to hand
 func drawCard(hand *[]string, deck *[]string) {
-	deckSize := len(*deck)
-	deck = deck[:deckSize-2]
-	hand = append(hand, deck[deckSize-1])
+	hand = append(hand, deck[deckSize-1]) //add last element from deck slice to the end of the hand
+	deck = deck[:len(*deck)-2] //should remove the last element from the deck
 }
 
 // Calculate the score of the hand
 func calculateScore(hand []string) int {
-	// YOU FILL IN HERE
+	sum := 0
+	aces := 0 //track aces and subtract them from the sum after if above 21
+	for i := 0; i < len(hand); i++ {
+		sum += valueMap[hand[i]] //get point value from valuemap constant defined above
+		if(hand[i] == "A") {
+			aces++ //count them and subtract later if needed
+		}
+	}
+	for sum > 21 && aces > 0 { //if the score is above 21 and an ace was in the hand
+		sum -= 10
+		aces--
+	}
 }
 
 // Print everyone's scores and hands
 func printStatus(playerCards, dealerCards []string) {
-	// YOU FILL IN HERE
+	fmt.Println("Player Cards:")
+	for i:= 0; i < len(playerCards); i++ { //print all player cards
+		fmt.Println(playerCards[i])
+	}
+	fmt.Println("Player Score: " + strconv.Itoa(calculateScore(playerCards)))
+	fmt.Println("Dealer Cards:")
+	for i:= 0; i < len(dealerCards); i++ { //print all dealer cards
+		fmt.Println(dealerCards[i])
+	}
+	fmt.Println("Dealer Score: " + strconv.Itoa(calculateScore(dealerCards)))
 }
 
 // Entry point and main game loop
